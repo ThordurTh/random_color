e84050c07c364b098aa8b2cb52e51d6a8f873d49
thordurf <thordur921@gmail.com>
2022-09-06 17:33:16 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function randomBackground() {
+    //Set the background to a random color
+}
+
+function randomColor() {
+    // returns a random rgb color object
+
+    // Generate a random color
+    // Return it in RGB to randomBackground
+}
+
+function rgbToCSS() {
+    // converts a rgb color object into a CSS color string
+
+    // Takes the random color RGB 
+    // Return it in a CSS color string to randomBackground
+}
